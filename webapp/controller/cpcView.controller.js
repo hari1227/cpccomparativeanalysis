8c4702d2b25f10d8a1aaa62603de91c9d3e9a7cb
Arvind Kumar Sharma <arvindk.sharma@chambal.in>
2023-04-05 17:40:26 +0000

    CPC ui app

---
+++ webapp/controller/cpcView.controller.js
@@ -0,0 +1,305 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    'sap/m/SearchField',
+    'sap/ui/model/type/String',
+    'sap/ui/table/Column',
+    'sap/m/Table'
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel, Filter, FilterOperator, SearchField, TypeString, UIColumn, mTable) {
+        "use strict";
+
+        return Controller.extend("cpccomparativeanalysis.controller.cpcView", {
+            onInit: function () {
+                // this.getOwnerComponent().getModel().read("/RFQEventCompDetailsProj", {
+                //     success: function (resp) {
+                //         this.showComparativeTable(resp.results);
+                //     }.bind(this)
+                // });
+
+                $.get({
+                    url: "/comparative-analysis/RFQEventCompDetailsProj",
+                    success: function(resp) {
+                        this.showComparativeTable(resp.value);
+                    }.bind(this),
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });
+                this.addSalesSummary();
+            },
+
+            addSalesSummary: function () {
+                var salesData = {
+                    "SalesSummary": [{
+                        "saleSummary": "Sales Plan (2021-2022)",
+                        "Quantity": 245
+                    },
+                    {
+                        "saleSummary": "Stock as on 01.04.2021",
+                        "Quantity": 5
+                    }]
+                };
+
+                var salesModel = new JSONModel(salesData);
+                var oSalesTable = this.getView().byId("salesTable");
+                oSalesTable.setModel(salesModel);
+                //oSalesTable.bindItems("/SalesSummary");
+
+                //Purchase Summary
+                var purData = {
+                    "PurchaseSummary": [{
+                        "saleSummary": "Stock price on 01.02.22",
+                        "Quantity": 188
+                    },
+                    {
+                        "saleSummary": "Purchase Price 22-23",
+                        "Quantity": 241
+                    }]
+                };
+
+                var purModel = new JSONModel(purData);
+                var oSalesTable = this.getView().byId("purchaseTable");
+                oSalesTable.setModel(purModel);
+            },
+
+            showComparativeTable: function (rfqItems) {
+                //Get Unique Vendors from the comparative Table
+                var lookup = {};
+                var items = rfqItems;//data.ComparativeAnalysis;
+                var vendorList = [{ vendorName: "itemTitle", vendorEmailId: "" }];
+
+                for (var item, i = 0; item = items[i++];) {
+                    var name = item.vendorName + item.eventID;
+                    //var eventID = item.eventID;
+
+                    if (!(name in lookup)) {
+                        lookup[name] = 1;
+                        vendorList.push(items[i]);
+                    }
+                }
+
+                //Get model unique Products
+                lookup = {};
+                var uniqueProducts = [];
+                for (var item, i = 0; item = items[i++];) {
+                    var name = item.itemTitle;
+
+                    if (!(name in lookup)) {
+                        lookup[name] = 1;
+                        uniqueProducts.push(name);
+                    }
+                }
+
+
+                var finalData = { "ComparativeAnalysis": [] };
+                for (var i = 0; i < uniqueProducts.length; i++) {
+                    var filteredData = rfqItems.filter(function (ca) {
+                        return ca.itemTitle == uniqueProducts[i];
+                    });
+                    var productData = {};
+                    productData.itemTitle = uniqueProducts[i];
+                    for (var filData of filteredData) {
+                        productData[filData.vendorName] = filData.finalFGPrice;
+
+                    }
+                    finalData.ComparativeAnalysis.push(productData);
+                }
+
+                // Logic for the SKU clause fields
+                var productClause = [{ id: "shelfLife", name: "Shelf Life" },
+                { id: "gstInput", name: "GST Input" },
+                { id: "creditDays", name: "Credit Days" }];
+                var productClauseObj;
+                for (var j in productClause) {
+                    productClauseObj = {};
+                    productClauseObj.itemTitle = productClause[j].name;
+                    for (var k in vendorList) {
+                        if (vendorList[k].vendorName !== "itemTitle") {
+                            productClauseObj[vendorList[k].vendorName] = vendorList[k][productClause[j].id];
+                        }
+                    }
+                    finalData.ComparativeAnalysis.push(productClauseObj);
+                    // productClausevendorList.itemTitle = productClause[j];
+
+                }
+
+                // Different Prices with Highes SKU quantity
+                var costFields = [
+                    { id: "bulkCost", name: "Bulk Cost" },
+                    { id: "pmCost", name: "PM Cost" },
+                    { id: "tollCharges", name: "Toll Charges" },
+                    { id: "freightIns", name: "Freight & Ins." },
+                    { id: "otherExpenses", name: "Misc." }
+                ];
+                var productCostObj;
+                for (var j in costFields) {
+                    productCostObj = {};
+                    productCostObj.itemTitle = costFields[j].name;
+                    for (var k in vendorList) {
+                        if (vendorList[k].vendorName !== "itemTitle") {
+                            //productClauseObj[vendorList[k].vendorName] = vendorList[k][productClause[j].id];
+                            var filteredCostData = rfqItems.filter(function (ca) {
+                                return ca.vendorMailId == vendorList[k].vendorMailId;
+                            });
+                            //Math.max(...filteredCostData.map(item => item.quantity))
+                            //var recordswithMaxQuan = Math.max.apply(Math, filteredCostData.map(function(item) { return item.quantity; }));
+                            var recordswithMaxQuan = filteredCostData.reduce((p, c) => p.quantity > c.quantity ? p : c);
+
+                            productCostObj[vendorList[k].vendorName] = recordswithMaxQuan[costFields[j].id]
+
+                        }
+
+                    }
+                    finalData.ComparativeAnalysis.push(productCostObj);
+                    // productClausevendorList.itemTitle = productClause[j];
+
+                }
+
+                var oTable = this.getView().byId("comparativeTable");
+                var columnName;
+                for (var j = 0; j < vendorList.length; j++) {
+                    if (j == 0) {
+                        columnName = "Particulars";
+                    } else {
+                        columnName = vendorList[j].vendorName + "(" + vendorList[j].eventID + ")";
+                    }
+                    var oColumn = new sap.m.Column("col" + j, {
+
+                        header: new sap.m.Label({
+                            text: columnName,
+                            wrapping: true
+                        })
+                    });
+                    oTable.addColumn(oColumn);
+                };
+
+                var oCell = [];
+                for (i = 0; i < vendorList.length; i++) {
+                    var cell1 = new sap.m.Text({
+                        text: "{" + vendorList[i].vendorName + "}"
+                    });
+                    oCell.push(cell1);
+                }
+                var aColList = new sap.m.ColumnListItem("aColList", {
+                    cells: oCell
+                });
+                var jsonModel = new JSONModel(finalData);
+                // console.log(data);
+                oTable.setModel(jsonModel);
+                oTable.bindItems("/ComparativeAnalysis", aColList);
+            },
+
+            // #region Value Help Dialog standard use case with filter bar without filter suggestions
+            onValueHelpRequested: function () {
+                this._oBasicSearchField = new SearchField();
+                if (!this.pDialog) {
+                    this.pDialog = this.loadFragment({
+                        name: "cpccomparativeanalysis.view.RFQValueHelp"
+                    });
+                }
+                this.pDialog.then(function (oDialog) {
+                    var oFilterBar = oDialog.getFilterBar();
+                    this._oVHD = oDialog;
+                    // Initialise the dialog with model only the first time. Then only open it
+                    if (this._bDialogInitialized) {
+                        // Re-set the tokens from the input and update the table
+                        oDialog.setTokens([]);
+                        //oDialog.setTokens(this._oMultiInput.getTokens());
+                        oDialog.update();
+
+                        oDialog.open();
+                        return;
+                    }
+                    this.getView().addDependent(oDialog);
+
+                    // Set key fields for filtering in the Define Conditions Tab
+                    oDialog.setRangeKeyFields([{
+                        label: "Internal ID",
+                        key: "internalId",
+                        type: "string",
+                        typeInstance: new TypeString({}, {
+                            maxLength: 7
+                        })
+                    }]);
+
+                    // Set Basic Search for FilterBar
+                    oFilterBar.setFilterBarExpanded(false);
+                    oFilterBar.setBasicSearch(this._oBasicSearchField);
+
+                    // Trigger filter bar search when the basic search is fired
+                    this._oBasicSearchField.attachSearch(function () {
+                        oFilterBar.search();
+                    });
+
+                    oDialog.getTableAsync().then(function (oTable) {
+
+                        //oTable.setModel(this.oProductsModel);
+
+                        // For Desktop and tabled the default table is sap.ui.table.Table
+                        if (oTable.bindRows) {
+                            // Bind rows to the ODataModel and add columns
+                            oTable.bindAggregation("rows", {
+                                path: "/RFQEvents",
+                                events: {
+                                    dataReceived: function () {
+                                        oDialog.update();
+                                    }
+                                }
+                            });
+                            oTable.addColumn(new UIColumn({ label: "RFQ", template: "Ebeln_Ebeln" }));
+                            oTable.addColumn(new UIColumn({ label: "Internal Id", template: "internalId" }));
+                            oTable.addColumn(new UIColumn({ label: "Title", template: "title" }));
+
+                        }
+
+                        // For Mobile the default table is sap.m.Table
+                        if (oTable.bindItems) {
+                            // Bind items to the ODataModel and add columns
+                            oTable.bindAggregation("items", {
+                                path: "/RFQEvents",
+                                template: new ColumnListItem({
+                                    cells: [new Label({ text: "{Ebeln_Ebeln}" }), new Label({ text: "{internalId}" }), new Label({ text: "{title}" })]
+                                }),
+                                events: {
+                                    dataReceived: function () {
+                                        oDialog.update();
+                                    }
+                                }
+                            });
+                            oTable.addColumn(new MColumn({ header: new Label({ text: "RFQ" }) }));
+                            oTable.addColumn(new MColumn({ header: new Label({ text: "Internal ID" }) }));
+                            oTable.addColumn(new MColumn({ header: new Label({ text: "Title" }) }));
+                        }
+                        oDialog.update();
+                    }.bind(this));
+
+                    //oDialog.setTokens(this._oMultiInput.getTokens());
+
+                    // set flag that the dialog is initialized
+                    this._bDialogInitialized = true;
+                    oDialog.open();
+                }.bind(this));
+            },
+
+            onRFQSelection: function () {
+                this.getView().byId("RFQEventFilter").setVisible(true);
+            },
+
+            onValueHelpOkPress: function (oEvent) {
+                var aTokens = oEvent.getParameter("tokens");
+                this.getView().byId("RFQEventFilterValueHelp").setTokens(aTokens);
+                this._oVHD.close();
+            },
+    
+            onValueHelpCancelPress: function () {
+                this._oVHD.close();
+            }
+
+        });
+    });
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"cpccomparativeanalysis/test/unit/controller/cpcView.controller"
+], function () {
+	"use strict";
+});
